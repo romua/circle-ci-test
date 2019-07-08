838886cc2c42985fd69d8d0263fde4bfd40e92fd
rchap <romafromukraine@gmail.com>
2019-07-08 15:09:49 +0300

    Fix tests

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { SwitchComponent } from '../../../shared/components/switch/switch.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent, SwitchComponent ],
+      imports: [ReactiveFormsModule]
     })
     .compileComponents();
   }));
